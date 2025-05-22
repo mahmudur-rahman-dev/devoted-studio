b132637f6465c33dc45266e55d80fbb1b067ecce
Mahmudur Rahman Promise <mahmudur.rahman.csedu@gmail.com>
2025-05-22 16:17:02 +0600

    phase 1 impl

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'ext.same-assets.com',
+      'placehold.co',
+      'oceandrive.com',
+      'www.minimallimited.com'
+    ],
+  },
+  experimental: {
+    typedRoutes: true,
+  },
+  eslint: {
+    dirs: ['app', 'components', 'lib'],
+  },
+}
+
+module.exports = nextConfig 
